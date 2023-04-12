cbc4d25b38836b1cc9555bd34e343260012a1cdd
SFlorin <stoflorin22@gmail.com>
2023-04-12 12:07:10 +0300

    Add assets

--- reactweatherapp/src/App.js
+++ reactweatherapp/src/App.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
 import Search from './Components/search/search.js';
+import CurrentWeather from './Components/current-weather/current-weather';
 
 function App() {
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split(' ');
   };
 
   return (
     <div className="main">
       <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather />
     </div>
   );
 }
---
+++ reactweatherapp/src/Components/current-weather/current-weather.js
@@ -0,0 +1,11 @@
+import './current-weather.css';
+
+const CurrentWeather = () => {
+  return (
+    <div className="currentweather">
+      <div className="left"></div>
+    </div>
+  );
+};
+
+export default CurrentWeather;
